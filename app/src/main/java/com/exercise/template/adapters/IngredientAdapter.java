d6f1c1fde24fc25d5cdf296250fe003c9b5444f0
Pandu <pandu.baraja@gmail.com>
2018-04-01 11:01:06 +0700

    create ingredient adapter n its item layout

---
+++ app/src/main/java/com/exercise/template/adapters/IngredientAdapter.java
@@ -0,0 +1,76 @@
+package com.exercise.template.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.exercise.template.R;
+import com.exercise.template.api.models.Ingredient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * File Created by pandu on 01/04/18.
+ */
+public class IngredientAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+
+    private List<Ingredient> data;
+
+    public IngredientAdapter(Context context) {
+        this.context = context;
+        data = new ArrayList<>();
+    }
+
+    public void setData(List<Ingredient> data) {
+        this.data.clear();
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater from = LayoutInflater.from(parent.getContext());
+        View view = from.inflate(R.layout.list_item_ingredient, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Ingredient ingredient = data.get(position);
+        ViewHolder viewHolder = ((ViewHolder) holder);
+
+        viewHolder.tvTitle.setText(context.getString(R.string.info_ingredient,
+                ingredient.getIngredient(), ingredient.getQuantity(), ingredient.getMeasure()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @BindView(R.id.tv_title)
+        TextView tvTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
