2c1de1ff495c97441b1cf3a836e6a8483163fdc9
Pandu <pandu.baraja@gmail.com>
2018-04-08 14:08:38 +0700

    load image using picasso

--- app/src/main/java/com/exercise/template/views/activities/detail/fragments/DetailFragment.java
+++ app/src/main/java/com/exercise/template/views/activities/detail/fragments/DetailFragment.java
@@ -4,29 +4,28 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import com.exercise.template.R;
 import com.exercise.template.adapters.IngredientAdapter;
 import com.exercise.template.adapters.StepAdapter;
-import com.exercise.template.api.models.Step;
 import com.exercise.template.views.activities.detail.DetailActivity;
 import com.exercise.template.views.activities.detail.viewmodels.DetailViewModel;
-import com.exercise.template.views.activities.main.viewmodels.MainViewModel;
 import com.exercise.template.views.base.BaseFragment;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import timber.log.Timber;
 
 /**
  * File Created by pandu on 31/03/18.
@@ -47,12 +46,17 @@ public class DetailFragment extends BaseFragment {
     @BindView(R.id.rv_step)
     RecyclerView rvStep;
 
+    @BindView(R.id.img_cake)
+    ImageView imgCake;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
     Unbinder unbinder;
 
     DetailViewModel detailViewModel;
+    @BindView(R.id.scroll_view)
+    ScrollView scrollView;
 
     public static DetailFragment newInstance() {
 
@@ -82,15 +86,27 @@ public class DetailFragment extends BaseFragment {
 
         detailViewModel = ViewModelProviders.of(getActivity()).get(DetailViewModel.class);
         detailViewModel.getRecipe().observe(getActivity(), recipe -> {
-            if(recipe != null) {
+            if (recipe != null) {
                 toolbar.setTitle(recipe.getName());
+
+                if (recipe.getImage() != null && !recipe.getImage().isEmpty()) {
+                    Picasso.with(getContext())
+                            .load(recipe.getImage())
+                            .error(R.drawable.ic_cake)
+                            .fit()
+                            .centerCrop()
+                            .into(imgCake);
+                }
 
                 ingredientAdapter.setData(recipe.getIngredients());
                 stepAdapter.setData(recipe.getSteps());
 
                 //SET DEFAULT
-                if(!recipe.getSteps().isEmpty())
-                detailViewModel.getSelectedStep().setValue(recipe.getSteps().get(0));
+                if (!recipe.getSteps().isEmpty())
+                    detailViewModel.getSelectedStep().setValue(recipe.getSteps().get(0));
+
+
+                scrollView.fullScroll(ScrollView.FOCUS_UP);
             }
         });
 
