8dafe6fc069ef32231a52e7fe18f2f6a3c7620f2
Pandu <pandu.baraja@gmail.com>
2018-04-08 14:15:33 +0700

    create TestApp, TestAppComponent, TestAppModule

---
+++ app/src/androidTest/java/com/exercise/template/TestApp.java
@@ -0,0 +1,25 @@
+package com.exercise.template;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+import timber.log.Timber;
+
+/**
+ * File Created by pandu on 08/04/18.
+ */
+public class TestApp extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(BuildConfig.DEBUG){
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    @Override
+    public AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerTestAppComponent.builder().create(this);
+    }
+}
