02454507869e5fc77d5535ec1986401222571387
Pandu <pandu.baraja@gmail.com>
2018-04-11 21:52:00 +0700

    create rawreader

---
+++ app/src/androidTest/java/com/exercise/template/RawReader.java
@@ -0,0 +1,32 @@
+package com.exercise.template;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * File Created by pandu on 11/04/18.
+ */
+public class RawReader {
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile(Context context, String filePath) throws Exception {
+        final InputStream stream = context.getResources().getAssets().open(filePath);
+
+        String ret = convertStreamToString(stream);
+        stream.close();
+        return ret;
+    }
+}
